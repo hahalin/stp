ec1e84d609a31ee19d96d5f4083a5d6edbae8b50
frank <frank.lin702960@cosmosbank.com.tw>
2013-06-13 16:43:59 +0800

    6/13 add password helper utility add unit test

--- application/controllers/utility.php
+++ application/controllers/utility.php
@@ -5,19 +5,76 @@ if (!defined('BASEPATH'))
 
 require_once(APPPATH.'libraries/crypt/Bcrypt.php');
 
+
 class utility extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
 		//$this -> load -> model('appcfg_model');
+		$this->load->helper('password');
+		$this->load->library('unit_test');
+	}
+	
+	public function utest()
+	{
+
+		
+		$test = 1 + 1;
+
+		$expected_result = 2;
+
+		$test_name = 'Adds one plus one';
+
+		//$this->unit->run($test, $expected_result, $test_name);
+		
+		
+		$r='$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq';
+		
+		$this->unit->run($this->gentest(), 'is_string','test is string');
+		
+		$this->unit->run($this->gentest(), $r,'test pawword');
+		
+		echo '<pre>';
+		//print_r( $result);
+		echo '</pre>';
+		
+		echo $this->unit->report();
+		
+		
 	}
 
-	public function genpwd()
+	public function addtest()
+	{
+		return (1+1);
+	}
+
+	public function showsalt()
+	{
+		$bcrypt = new Bcrypt(15);
+		print $bcrypt->getSalt();
+	}
+
+	public function gentest()
+	{
+	
+		$options = array(
+		    'cost' => 7,
+		    'salt' => 'BCryptRequires22Chrcts',
+		);
+		$result=password_hash("rasmuslerdorf", PASSWORD_BCRYPT, $options)."\n";
+		
+		return trim($result);
+		
+		
+	}
+	
+	public function genpwd($pwd='pwd')
 	{
 		$bcrypt = new Bcrypt(15);
 
-		$hash = $bcrypt->hash('pwd');
+		$hash = $bcrypt->hash($pwd);
 		
+		//return $hash;
 		print $hash;		
 	}
 
